669808d1ef79bd4af6559e3d4fb12cbbe7a7e609
Eelolo <Eelolo@yandex.ru>
2021-01-09 22:51:41 +0300

    registration test case

--- booking_app/tests.py
+++ booking_app/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class RegistrationTestCase(APITestCase):
+    def test_registration(self):
+        data = {
+            'username': 'testcase',
+            'email': 'testcase@test.test',
+            'password': '321SomePSWD123'
+        }
+        response = self.client.post('/users/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
