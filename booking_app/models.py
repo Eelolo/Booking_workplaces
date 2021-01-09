4df42a2b9a6efb3cabb8952f0091663983960a44
Eelolo <Eelolo@yandex.ru>
2021-01-09 20:02:21 +0300

    office model

--- booking_app/models.py
+++ booking_app/models.py
@@ -2,3 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+class Office(models.Model):
+    workplaces = models.IntegerField(
+        default=5
+    )
