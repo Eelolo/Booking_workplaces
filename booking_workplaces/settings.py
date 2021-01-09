bb3035e94dfe6de3f805a01a8e62485d07420614
Eelolo <Eelolo@yandex.ru>
2021-01-09 20:16:43 +0300

    configured settings

--- booking_workplaces/settings.py
+++ booking_workplaces/settings.py
@@ -47,6 +47,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'booking_app',
 ]
 
 MIDDLEWARE = [
