2da3fa7a6f53e0f61365470980094f20ebd9a5af
Eelolo <Eelolo@yandex.ru>
2021-01-09 22:05:13 +0300

    error 404 handler

--- booking_app/utils/views.py
+++ booking_app/utils/views.py
@@ -13,3 +13,17 @@ def error_400(request, exception):
     response.status_code = 400
 
     return response
+
+
+def error_404(request, exception):
+    data = {
+        'Error': {
+            'status_code': 404,
+            'error_message': 'The endpoint not found.',
+            'code': 'NOT_FOUND'
+        }
+    }
+    response = JsonResponse(data=data)
+    response.status_code = 404
+
+    return response
