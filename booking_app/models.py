a82d0c00c7e84d2772649972072e835a7a096ea0
Eelolo <Eelolo@yandex.ru>
2021-01-09 20:03:25 +0300

    workplace model

--- booking_app/models.py
+++ booking_app/models.py
@@ -3,7 +3,19 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Office(models.Model):
     workplaces = models.IntegerField(
         default=5
+    )
+
+
+class Workplace(models.Model):
+    office = models.ForeignKey(
+        Office,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    price = models.IntegerField(
+        default=1000
     )
