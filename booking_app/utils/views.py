5ac7a64ffbaf6c2e6702739adbc2df6d7f1410b3
Eelolo <Eelolo@yandex.ru>
2021-01-09 22:05:36 +0300

    error 500 handler

--- booking_app/utils/views.py
+++ booking_app/utils/views.py
@@ -27,3 +27,17 @@ def error_404(request, exception):
     response.status_code = 404
 
     return response
+
+
+def error_500(request):
+    data = {
+        'Error': {
+            'status_code': 500,
+            'error_message': 'Internal Server Error.',
+            'code': 'INTERNAL_ERROR'
+        }
+    }
+    response = JsonResponse(data=data)
+    response.status_code = 500
+
+    return response
