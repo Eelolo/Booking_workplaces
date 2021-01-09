0a4b18d618cd8e2f738b1496f5c7a2f8ee93711a
Eelolo <Eelolo@yandex.ru>
2021-01-09 22:27:07 +0300

    fix

--- booking_app/mixins.py
+++ booking_app/mixins.py
@@ -1,4 +1,4 @@
-from rest_framework.mixins import CreateModelMixinn, UpdateModelMixin
+from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
 from booking_app.validations import Validations
 from rest_framework.response import Response
 from rest_framework import status
