4d5b49404c899bcf8e6b8ea03534e380466387b0
Eelolo <Eelolo@yandex.ru>
2021-01-09 20:10:51 +0300

    reservations serializer

--- booking_app/serializers.py
+++ booking_app/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from booking_app.models import Office, Workplace, Reservation
+from datetime import date
+from datetime import timedelta
 
 
 class OfficeDetailSerializer(serializers.ModelSerializer):
@@ -25,3 +27,24 @@ class WorkplaceDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workplace
         fields = '__all__'
+
+
+class ReservationDetailSerializer(serializers.ModelSerializer):
+    reservation_days = serializers.SerializerMethodField('get_reservation_days_list')
+
+    class Meta:
+        model = Reservation
+        fields = (
+            'id', 'office', 'workplace',
+            'initial_day', 'reservation_ends', 'reservation_days', 'user'
+        )
+
+    def get_reservation_days_list(self, reservation_model):
+        initial_day = reservation_model.initial_day
+        reservation_ends = reservation_model.reservation_ends
+        reservation_days = []
+
+        for delta in range((reservation_ends - initial_day).days + 1):
+            reservation_days.append(date.isoformat(initial_day + timedelta(days=delta)))
+
+        return reservation_days
