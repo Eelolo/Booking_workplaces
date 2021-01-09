e748b754f91db1f2bc2a1af67a5865fa5507656b
Eelolo <Eelolo@yandex.ru>
2021-01-09 20:22:33 +0300

    office list view

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from booking_app.views import OfficeCreateView
+from booking_app.views import OfficeCreateView, OfficeListView
 
 urlpatterns = [
     path('office/create/', OfficeCreateView.as_view()),
+    path('offices/', OfficeListView.as_view()),
 ]
--- booking_app/views.py
+++ booking_app/views.py
@@ -1,7 +1,14 @@
 from rest_framework import generics
 from booking_app.serializers import OfficeDetailSerializer
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from booking_app.models import Office
 
 class OfficeCreateView(generics.CreateAPIView):
     serializer_class = OfficeDetailSerializer
     permission_classes = (IsAuthenticated, IsAdminUser)
+
+
+class OfficeListView(generics.ListAPIView):
+    serializer_class = OfficeDetailSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = Office.objects.all()
