2ecae4445c822db73782a8933dd7f3a93b42934d
Eelolo <Eelolo@yandex.ru>
2021-01-09 22:55:44 +0300

    office test cases

--- booking_app/tests.py
+++ booking_app/tests.py
@@ -3,6 +3,9 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+from booking_app.models import Office, Workplace
+from django.urls import reverse
+
 
 
 class RegistrationTestCase(APITestCase):
@@ -50,3 +53,64 @@ class AuthenticationTestCase(APITestCase):
         response = self.client.get(self.get_users_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data[0]['username'], self.user.username)
+
+
+class OfficeTestCases(APITestCase):
+    create_url = '/office/create/'
+    list_url = '/offices/'
+
+    def setUp(self):
+        self.user = User.objects.create_superuser(username='super_testcase', password='testcase')
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_create_office(self):
+        data = {
+            'workplaces': 5
+        }
+
+        response = self.client.post(self.create_url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        office = Office.objects.all()[0]
+        self.assertEqual(office.pk, 1)
+        self.assertEqual(office.workplaces, data['workplaces'])
+        self.assertEqual(response.data['workplaces_id'], [])
+
+        response = self.client.post(self.create_url, data)
+        self.assertEqual(response.data['id'], 2)
+
+    def test_offices_list(self):
+        Office.objects.create(workplaces=5)
+        Office.objects.create(workplaces=5)
+
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(isinstance(list(response.data), list), True)
+        self.assertEqual(isinstance(dict(list(response.data)[1]), dict), True)
+
+    def test_edit_office(self):
+        office = Office.objects.create(workplaces=5)
+
+        response = self.client.get(reverse('OfficeEditView', kwargs={'pk': office.pk}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], Office.objects.all()[0].pk)
+
+        response = self.client.patch(reverse('OfficeEditView', kwargs={'pk': office.pk}), data={'workplaces': 10})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        office = Office.objects.all().get(pk=office.pk)
+        self.assertEqual(office.workplaces, 10)
+
+        response = self.client.delete(reverse('OfficeEditView', kwargs={'pk': office.pk}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        workplaces = Workplace.objects.all()
+        self.assertEqual(list(workplaces), [])
+
+    def test_office_permissions(self):
+        testcase_user = User.objects.create_user(username='testcase', password='testcase')
+        self.client.force_authenticate(user=testcase_user)
+
+        response = self.client.post(self.create_url, {'workplaces': 5})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        response = self.client.delete(reverse('OfficeEditView', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
